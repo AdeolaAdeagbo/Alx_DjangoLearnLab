f821a9edc18012f1672d14bfea50c24f9e6d99c8
Boluwatife Zainab Adeagbo <adeagbozain50@gmail.com>
2025-11-15 20:46:35 +0100

    Implement custom user model, admin, and security settings

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required, login_required
 from django.http import HttpResponseForbidden
 from .models import Book
+from .forms import ExampleForm
+
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
