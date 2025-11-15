2defd919ff243d12e2d461c86db4d491c7cb09f2
Boluwatife Zainab Adeagbo <adeagbozain50@gmail.com>
2025-11-15 18:55:43 +0100

    Task 4: Add book permission URLs in LibraryProject folder

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,13 +12,12 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
-    # Role-based access views
+    # Role-based views
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
-]
 
-urlpatterns += [
+    # Custom permissions for Task 4
     path('book/add/', views.add_book, name='add_book'),
     path('book/<int:pk>/edit/', views.edit_book, name='edit_book'),
     path('book/<int:pk>/delete/', views.delete_book, name='delete_book'),
