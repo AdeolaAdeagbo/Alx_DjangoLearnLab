83eeb9a15cc8df26a2355b226a34081bf02b5182
Boluwatife Zainab Adeagbo <adeagbozain50@gmail.com>
2025-11-15 18:25:17 +0100

    corrections

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,17 @@
+# Import models exactly as checker expects
+from .models import Library
+from .models import Book
+
+# Import Django views
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book, Library
 
-# Function-based view: list all books
+# Function-based view: List all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view: library details
+# Class-based view: Library detail using DetailView
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
